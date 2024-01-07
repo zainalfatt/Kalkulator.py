136aa62f612fec3d1148201ba04eee7b6c9cfa37
Zainal <105146599+zainalfatt@users.noreply.github.com>
2024-01-07 17:02:00 +0700

    Update Kalkulator.py

--- Kalkulator.py
+++ Kalkulator.py
@@ -17,32 +17,32 @@ def bagi(x, y):
 def modulus(x, y):
     return x % y
 
-print("Select Operation.")
+print("Pilih Operasi.")
 print("1. Penjumlahan")
 print("2. Pengurangan")
 print("3. Perkalian")
 print("4. Pembagian")
 print("5. Modulus")
 
-choice = input("Enter Choice (1/2/3/4/5) : ")
+choice = input("Masukan Pilihanmu (1/2/3/4/5) : ")
 
 num1 = int(input("Masukan angka pertama : "))
 num2 = int(input("Masukan angka kedua : "))
 
 if choice == '1':
-    print("Result =>",num1,"+",num2,"=", tambah(num1, num2))
+    print("Hasil => ",num1," + ",num2," = ", tambah(num1, num2))
 
 elif choice == '2':
-    print("Result =>",num1,"-",num2,"=", kurang(num1, num2))
+    print("Hasil => ",num1," - ",num2," = ", kurang(num1, num2))
 
 elif choice == '3':
-    print("Result =>",num1,"*",num2,"=", kali(num1, num2))
+    print("Hasil => ",num1," * ",num2," = ", kali(num1, num2))
 
 elif choice == '4':
-    print("Result =>",num1,"/",num2,"=", bagi(num1, num2))
+    print("Hasil => ",num1," / ",num2," = ", bagi(num1, num2))
 
 elif choice =='5':
-    print("Result =>",num1,"%",num2,"=",modulus(num1, num2))
+    print("Hasil => ",num1," % ",num2,"=",modulus(num1, num2))
 
 else:
     print("Invalid input")
